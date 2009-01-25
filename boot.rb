9d6891f0aa2d154cb68d17a01dc6fdeb3ab80353
Pavel Gorbokon <pahanix@gmail.com>
2009-01-25 23:58:46 +0200

    fixed misspelling in log filename, replaced magic constant __FILE__ with Dir.pwd

--- boot.rb
+++ boot.rb
@@ -4,9 +4,9 @@ require 'dm-validations'
 require 'dm-timestamps'
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/dictionary.sqlite3")
-DataObjects::Sqlite3.logger = DataObjects::Logger.new('dictionaly.log', :debug) # :off, :fatal, :error, :warn, :info, :debug
+DataObjects::Sqlite3.logger = DataObjects::Logger.new('dictionary.log', :debug) # :off, :fatal, :error, :warn, :info, :debug
 
-Dir["#{File.dirname(__FILE__)}/{translator,models}/**/*.rb"].each { |f| require f }
+Dir["#{Dir.pwd}/{translator,models}/**/*.rb"].each { |f| require f }
 
 # DataMapper.auto_migrate!
 DataMapper.auto_upgrade!
