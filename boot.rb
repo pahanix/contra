9df00ce23856881205c03f783d17863c35bf300d
pavel.gorbokon@railsware.com <pahanix@gmail.com>
2009-01-26 00:28:07 +0200

    db file moved to db/ folder log file moved to log/ folder

--- boot.rb
+++ boot.rb
@@ -3,8 +3,8 @@ require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/dictionary.sqlite3")
-DataObjects::Sqlite3.logger = DataObjects::Logger.new('dictionary.log', :debug) # :off, :fatal, :error, :warn, :info, :debug
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/dictionary.sqlite3")
+DataObjects::Sqlite3.logger = DataObjects::Logger.new('log/dictionary.log', :debug) # :off, :fatal, :error, :warn, :info, :debug
 
 Dir["#{Dir.pwd}/{translator,models}/**/*.rb"].each { |f| require f }
 
