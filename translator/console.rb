a8b4c41d5fe4e7cdf287b7461cd97b1229a8eecd
Pavel Gorbokon aka pahanix <pavel.gorbokon@railsware.com>
2009-01-27 01:11:14 +0200

    * added readline support (you cat edit input line and have a session history of line) * added storing history in a .history file

--- translator/console.rb
+++ translator/console.rb
@@ -1,3 +1,5 @@
+require 'readline'
+
 module Translator
   class Console  
     # cattr_accessor
@@ -15,11 +17,11 @@ module Translator
 
     def self.run
       raise "Console should have a translation provider. Please configure console with #{self}.config { |config| ... }" unless provider
+      load_history!
       loop do
-        ($stdout << prompt).flush
-        unit = gets.strip # strip removes "\n" at the end of the unit
+        unit = Readline::readline(prompt, true)
         break if terminal?(unit)
-        $stdout << execute(unit)
+        puts execute(unit)
       end
     end
 
@@ -32,9 +34,49 @@ module Translator
     rescue SocketError => e
       "Cannot connect to the host #{provider.host}\n"
     end
+
+    #
+    # Some ideas and snippents for Readline::HISTORY are taken from Wirble gem 0.1.2
+    #
+
+    @@cfg = {
+      :path => "./.history",
+      :max_size => 1000,
+      :perms => File::WRONLY | File::CREAT | File::TRUNC
+    }
+
+    def self.cfg(key)
+      @@cfg[key.intern]
+    end
+
+    def self.history_lines
+      real_path = File.expand_path(cfg('path'))
+      File.open(real_path, 'w+'){ |file| file } unless File.exist?(real_path)
+      lines = File.readlines(real_path).map { |line| line.chomp }
+    end
     
+    def self.load_history!
+      Readline::HISTORY.push(*history_lines)
+    end
+    
+    def self.save_history!
+      path, max_size, perms = %w{path max_size perms}.map { |v| cfg(v) }
+      
+      # reverse.uniq.reverse uniqifies from last to first instead of first to last as usual
+      lines = (history_lines + Readline::HISTORY.to_a).reverse.uniq.reverse
+      lines = lines[-max_size, -1] if lines.size > max_size
+      
+      # write the history file
+      real_path = File.expand_path(path)
+      File.open(real_path, perms) { |fh| fh.puts lines }
+    end
+        
     def self.terminal?(unit)
-      [":stop", ":exit", ":quit", ":term"].include?(unit.downcase)
+      if [":stop", ":exit", ":quit", ":term"].include?(unit.downcase)
+        save_history!
+        return true
+      end
+      false
     end
     
     def self.command?(unit)
@@ -43,7 +85,7 @@ module Translator
     
     def self.execute(unit)
       if command?(unit)
-        "EXECUTED COMMAND #{command}"
+        "EXECUTED COMMAND #{unit}"
       else
         translate(unit)
       end
