515b308af9dfae71242327875c87c29284b51add
Pavel Gorbokon aka pahanix <pavel.gorbokon@railsware.com>
2009-01-28 21:02:03 +0200

    * words history separated into History class * added saving history at exit (^C support) through Kernel.at_exit { save_history }

--- translator/console.rb
+++ translator/console.rb
@@ -1,5 +1,3 @@
-require 'readline'
-
 module Translator
   class Console  
     # cattr_accessor
@@ -15,9 +13,13 @@ module Translator
       yield self
     end
 
-    def self.run
+    def self.init
       raise "Console should have a translation provider. Please configure console with #{self}.config { |config| ... }" unless provider
-      load_history!
+      History.new
+    end
+
+    def self.run
+      init
       loop do
         unit = Readline::readline(prompt, true)
         break if terminal?(unit)
@@ -34,49 +36,9 @@ module Translator
     rescue SocketError => e
       "Cannot connect to the host #{provider.host}\n"
     end
-
-    #
-    # Some ideas and snippents for Readline::HISTORY are taken from Wirble gem 0.1.2
-    #
-
-    @@cfg = {
-      :path => "./.history",
-      :max_size => 1000,
-      :perms => File::WRONLY | File::CREAT | File::TRUNC
-    }
-
-    def self.cfg(key)
-      @@cfg[key.intern]
-    end
-
-    def self.history_lines
-      real_path = File.expand_path(cfg('path'))
-      File.open(real_path, 'w+'){ |file| file } unless File.exist?(real_path)
-      lines = File.readlines(real_path).map { |line| line.chomp }
-    end
     
-    def self.load_history!
-      Readline::HISTORY.push(*history_lines)
-    end
-    
-    def self.save_history!
-      path, max_size, perms = %w{path max_size perms}.map { |v| cfg(v) }
-      
-      # reverse.uniq.reverse uniqifies from last to first instead of first to last as usual
-      lines = (history_lines + Readline::HISTORY.to_a).reverse.uniq.reverse
-      lines = lines[-max_size, -1] if lines.size > max_size
-      
-      # write the history file
-      real_path = File.expand_path(path)
-      File.open(real_path, perms) { |fh| fh.puts lines }
-    end
-        
     def self.terminal?(unit)
-      if [":stop", ":exit", ":quit", ":term"].include?(unit.downcase)
-        save_history!
-        return true
-      end
-      false
+      [":stop", ":exit", ":quit", ":term"].include?(unit.downcase)
     end
     
     def self.command?(unit)
---
+++ translator/history.rb
@@ -0,0 +1,46 @@
+require 'readline'
+#
+# Some ideas and snippents for Readline::HISTORY are taken from Wirble gem 0.1.2
+#
+module Translator
+  class History
+    DEFAULTS = {
+      :path => "./.history",
+      :max_size => 1000,
+      :perms => File::WRONLY | File::CREAT | File::TRUNC
+    }
+
+    def initialize(opts={})
+      @options = DEFAULTS.merge(opts || {})
+      return unless defined? Readline::HISTORY
+      load_history
+      Kernel.at_exit { save_history }
+    end
+
+    def cfg(key)
+      @options[key.intern]
+    end
+
+    def history_lines
+      real_path = File.expand_path(cfg('path'))
+      File.open(real_path, 'w+'){ |file| file } unless File.exist?(real_path)
+      lines = File.readlines(real_path).map { |line| line.chomp }
+    end
+    
+    def load_history
+      Readline::HISTORY.push(*history_lines)
+    end
+    
+    def save_history
+      path, max_size, perms = %w{path max_size perms}.map { |v| cfg(v) }
+      
+      # reverse.uniq.reverse uniqifies from last to first instead of first to last as usual
+      lines = (history_lines + Readline::HISTORY.to_a).reverse.uniq.reverse
+      lines = lines[-max_size, -1] if lines.size > max_size
+      
+      # write the history file
+      real_path = File.expand_path(path)
+      File.open(real_path, perms) { |fh| fh.puts lines }
+    end
+  end
+end
